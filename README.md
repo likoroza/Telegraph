6d14c806208ea7cef821af6e535b773bc57b68fd
likoroza <creeperani12@gmail.com>
2026-01-15 13:32:02 +0200

    Added character decoder.

--- README.md
+++ README.md
@@ -6,20 +6,35 @@ This is a "telegraph" project used to communicate Morse Code between devices. Th
 # Components
 You will need:
 - An **Arduino UNO R3** (or a clone).
-- A ***passive* buzzer.**
-- A pushbutton.
+- A ***passive* buzzer.***
+- An **LED.***
+- A **220Ω resisor** (R1).
+- A **pushbutton.**
+
+*Optional.
 
 ## Setup
 **1. Setup the wiring:**
 
 |Terminal 1|Terminal 2|
 |----------|----------|
-|Buzzer terminal 1|Arduino D11 **(BUZZER_PIN)**|
-|Buzzer terminal 2|Arduino GND|
 |Pushbutton terminal 1|Arduino GND|
 |Pushbutton terminal 2|Arduino D2 **(PADDLE_PIN)**|
+|Buzzer terminal 1|Arduino D11 **(BUZZER_PIN)**|
+|Buzzer terminal 2|Arduino GND|
+|LED Anode|Arduino D10 **(LIGHT_PIN)**|
+|R1 terminal 1|LED Cathode|
+|R1 terminal 2| Arduino GND|
 <hr>
 
 **2. Install the firmware:**
 
 Flash `Telegraph.ino` on your Arduino.
+
+Before flashing, you may change:
+- `LIGHT_OUTPUT` - Should the led mirror your paddle?
+- `SOUND_OUTPUT` - Should the buzzer mirror your paddle?
+- `DASH_THRESHOLD` - How much milliseconds of pressing makes for a dash rather than a dot?
+- `dealWithCharacter()` implemntation - What should the code do with the character once it finishes?
+
+Hope you enjoy! :3
--- Telegraph.ino
+++ Telegraph.ino
@@ -1,19 +1,62 @@
 int PADDLE_PIN = 2;
 int BUZZER_PIN = 11; 
+int LIGHT_PIN = 10;
 
-int start_press;
+bool LIGHT_OUTPUT = true;
+bool SOUND_OUTPUT = true;
+int DASH_THRESHOLD = 150;
+
+int time_at_state_start;
+bool is_measuring_pressed = false; 
+
+String currentCharacter;
 
 void setup()
 {
 	pinMode(PADDLE_PIN, INPUT_PULLUP);
     pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(LIGHT_PIN, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    bool padelState = !digitalRead(PADDLE_PIN);
-    buzz(padelState);
+    bool paddleState = !digitalRead(PADDLE_PIN);
+    
+    if (SOUND_OUTPUT)
+    {
+        buzz(paddleState);
+    }
+    
+
+    if (LIGHT_OUTPUT) {
+        digitalWrite(LIGHT_PIN, paddleState);
+    }
+
+    if (paddleState && !is_measuring_pressed) {
+        time_at_state_start = millis();
+        is_measuring_pressed = true;
+    }
+
+    if (!paddleState) {
+        int stateTime = millis() - time_at_state_start;
+
+        if (is_measuring_pressed) {
+
+            currentCharacter += stateTime < DASH_THRESHOLD ? "." : "-";
+            delay(1);
+
+            is_measuring_pressed = false;
+            time_at_state_start = millis();
+            return;
+        }
+
+        if (stateTime >= (DASH_THRESHOLD * 3) && currentCharacter != "") {
+            dealWithCharacter(currentCharacter);
+            currentCharacter = "";
+        }
+        
+    }
 }
 
 void buzz(bool state) {
@@ -24,4 +67,8 @@ void buzz(bool state) {
     else {
         noTone(BUZZER_PIN);
     }
+}
+
+void dealWithCharacter(String currentCharacter) {
+    //TODO: Implement method
 }
